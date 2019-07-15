af003ca2d8de831a08c327abfbc55e423cd25642
wanghui <wanghui2@mafengwo.com>
2019-07-15 17:34:30 +0800

    做个小实验，没用啊喂还得自己写

--- app/src/main/java/com/example/wanghui/kotlin/Main2Activity.kt
+++ app/src/main/java/com/example/wanghui/kotlin/Main2Activity.kt
@@ -2,7 +2,16 @@ package com.example.wanghui.kotlin
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.util.Log
+import android.view.ViewGroup
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main2.*
+import org.jetbrains.anko.UI
+import org.jetbrains.anko.dip
+import org.jetbrains.anko.find
+import org.jetbrains.anko.textView
 
 
 class Main2Activity : AppCompatActivity() {
@@ -14,7 +23,47 @@ class Main2Activity : AppCompatActivity() {
     }
 
     private fun testScroller() {
+        recyclerView1.layoutManager = LinearLayoutManager(this)
+        recyclerView2.layoutManager = LinearLayoutManager(this)
+        recyclerView1.adapter = Adapter(createList("1"))
+        recyclerView2.adapter = Adapter(createList("2"))
+    }
 
+    private fun createList(sub: String): List<String>{
+        return (0..100).map {
+            "$sub 测试 $it "
+        }
+    }
+
+    class Adapter(val list: List<String>): RecyclerView.Adapter<MViewHolder>(){
+        override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): MViewHolder {
+            return MViewHolder(parent!!)
+        }
+
+        override fun getItemCount(): Int {
+            return list.size
+        }
+
+        override fun onBindViewHolder(holder: MViewHolder?, position: Int) {
+            holder?.onBindViewHolder(list.get(position))
+        }
+
+    }
+
+    class MViewHolder(parent: ViewGroup): RecyclerView.ViewHolder(createView(parent)){
+        companion object{
+            fun createView(parent: ViewGroup) = parent.context.UI {
+                textView {
+                    id = R.id.text
+                    layoutParams = ViewGroup.LayoutParams(dip(300),  dip(150))
+                }
+            }.view
+        }
+
+        fun onBindViewHolder(text: String){
+            Log.d("wh", "wh------onbind $adapterPosition ${this.hashCode()}")
+            itemView.find<TextView>(R.id.text).text = "HHHHHHHHH$text"
+        }
 
     }
 }
--- app/src/main/java/com/example/wanghui/kotlin/ui/scroll/ScrollDemo.java
+++ app/src/main/java/com/example/wanghui/kotlin/ui/scroll/ScrollDemo.java
@@ -1,24 +1,11 @@
 package com.example.wanghui.kotlin.ui.scroll;
 
 import android.content.Context;
-import android.content.Intent;
-import android.graphics.Canvas;
-import android.graphics.ColorFilter;
-import android.graphics.Paint;
-import android.graphics.drawable.Drawable;
-import android.support.annotation.IntRange;
-import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
-import android.widget.Button;
 import android.widget.FrameLayout;
-import android.widget.ScrollView;
-
-import com.example.wanghui.kotlin.Main2Activity;
-import com.example.wanghui.kotlin.MainActivity;
 
 import java.util.Timer;
 import java.util.TimerTask;
@@ -47,13 +34,6 @@ public class ScrollDemo extends FrameLayout {
 
     @Override
     public void computeScroll() {
-        new Timer().schedule(new TimerTask() {
-            @Override
-            public void run() {
-
-            }
-        }, 2000);
-
         int a = 0;
         switch (a){
             case MotionEvent.ACTION_DOWN:
